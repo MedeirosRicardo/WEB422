0ba30b4d1dca5669e4a3f290996309282bc3f433
Ricardo Medeiros <medeiros.ricardo@outlook.com>
2020-02-11 07:15:15 -0500

    Step 1 - done

--- assignment-3/src/App.js
+++ assignment-3/src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
+/*********************************************************************************
+*  WEB422 – Assignment 3
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Ricardo Medeiros Student ID: 135745180 Date: February 10, 2020
+*  Heroku Link: 
+*
+********************************************************************************/
+
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recentlyViewed: [],
+      searchId: ""
+    };
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
-  );
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
