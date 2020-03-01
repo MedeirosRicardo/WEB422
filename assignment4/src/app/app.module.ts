9773d165b339840be39802de7ea5e47827cb9f82
Ricardo Medeiros <medeiros.ricardo@outlook.com>
2020-03-01 01:59:44 -0500

    Assignment 4 - Step 2 done

--- assignment4/src/app/app.module.ts
+++ assignment4/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { BlogComponent } from './blog/blog.component';
+import { PostComponent } from './post/post.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    BlogComponent,
+    PostComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
