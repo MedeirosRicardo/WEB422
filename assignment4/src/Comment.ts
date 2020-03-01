719b4cae96311b78aea7d6c57be73a6b93a28e72
Ricardo Medeiros <medeiros.ricardo@outlook.com>
2020-03-01 12:06:55 -0500

    Assignment 4 - Step 4 done

---
+++ assignment4/src/Comment.ts
@@ -0,0 +1,6 @@
+export class Comment {
+    _id: string;
+    author: string;
+    comment: string;
+    date: string;
+}
--- assignment4/src/app/blog/blog.component.ts
+++ assignment4/src/app/blog/blog.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+// Import Mock Data
+import blogData from '../../blogData.json';
+import { BlogPost } from '../../BlogPost'
+
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
   styleUrls: ['./blog.component.css']
 })
 export class BlogComponent implements OnInit {
+
+  blogPosts: Array<BlogPost> = blogData;
 
   constructor() { }
 
