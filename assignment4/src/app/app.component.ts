7ac7336b4299ef00ff562bc48f14c522e36ec85e
Ricardo Medeiros <medeiros.ricardo@outlook.com>
2020-03-01 02:23:58 -0500

    Assignment 4 - Step 3 done

--- assignment4/src/app/app.component.ts
+++ assignment4/src/app/app.component.ts
@@ -1,3 +1,13 @@
+/*********************************************************************************
+*  WEB422 – Assignment 4
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Ricardo Medeiros Student ID: 135745180 Date: February 29, 2020
+*
+********************************************************************************/
+
 import { Component } from '@angular/core';
 
 @Component({
