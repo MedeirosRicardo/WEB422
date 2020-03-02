d31366f32d96643c3fd51f55ff4084839793a209
Ricardo Medeiros <medeiros.ricardo@outlook.com>
2020-03-02 16:47:36 -0500

    Assignment 4 - fixed aside components

--- assignment4/src/app/post/post.component.ts
+++ assignment4/src/app/post/post.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+// Import Mock Data
+import blogData from '../../blogData.json';
+import { BlogPost } from '../../BlogPost'
+
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
+
+  blogPosts: Array<BlogPost> = blogData;
 
   constructor() { }
 
