8b3f41e539c017cb9933db5037e1a57668514519
Ricardo Medeiros <medeiros.ricardo@outlook.com>
2020-03-10 13:36:06 -0400

    getPosts - done

--- assignment5/blog/src/app/post.service.ts
+++ assignment5/blog/src/app/post.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BlogPost } from '../BlogPost';
+import { Observable } from 'rxjs';
+
+const perPage = 6;
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // Get posts per page
+  getPosts(page, tag, category): Observable<BlogPost[]> {
+
+    // Default apiURL
+    let apiURL: string = `https://arnin-blogapi.herokuapp.com/api/posts?page=${page}&perPage=${perPage}`;
+
+    // Add tag to apiURL
+    if (tag !== null || tag !== undefined) {
+      apiURL = apiURL + `&${tag}`;
+    }
+
+    // Add category to apiURL
+    if (category !== null || category !== undefined) {
+      apiURL = apiURL + `&category${category}`;
+    }
+
+    return this.http.get<BlogPost[]>(apiURL);
+  }
 }
