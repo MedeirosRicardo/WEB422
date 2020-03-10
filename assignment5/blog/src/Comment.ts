6223017ab63aaec1de6be3d39d053eebcd5806b6
Ricardo Medeiros <medeiros.ricardo@outlook.com>
2020-03-10 12:08:50 -0400

    post.service init

--- assignment5/blog/src/Comment.ts
+++ assignment5/blog/src/Comment.ts
@@ -1,5 +1,4 @@
 export class Comment {
-    _id: string;
     author: string;
     comment: string;
     date: string;
--- assignment5/blog/src/app/app.module.ts
+++ assignment5/blog/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -30,7 +32,8 @@ import { PostDataComponent } from './post-data/post-data.component';
     LatestPostComponent,
     CategoriesComponent,
     TagsComponent,
-    PostDataComponent
+    PostDataComponent,
+    HttpClientModule
   ],
   imports: [
     BrowserModule,
