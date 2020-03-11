6c1d9abb9d61552ad9be0b05e334e3665ed756e1
Ricardo Medeiros <medeiros.ricardo@outlook.com>
2020-03-11 10:57:02 -0400

    Assignment 5 - Step 6 - done

--- assignment5/blog/src/app/categories/categories.component.ts
+++ assignment5/blog/src/app/categories/categories.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-categories',
@@ -7,20 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriesComponent implements OnInit {
 
-  categories: Array<any> = [
-    { cat: "Crime", num: 2 },
-    { cat: "Comedy", num: 1 },
-    { cat: "Musical", num: 1 },
-    { cat: "Adventure", num: 2 },
-    { cat: "Drama", num: 2 },
-    { cat: "Action", num: 2 },
-    { cat: "Documentary", num: 1 },
-    { cat: "Thriller", num: 1 }
-  ];
+  categories: Array<any>;
 
-  constructor() { }
+  private cats;
+
+  constructor(private data: PostService) { }
 
   ngOnInit(): void {
+    this.cats = this.data.getCategories().subscribe(data => this.categories = data);
   }
 
 }
