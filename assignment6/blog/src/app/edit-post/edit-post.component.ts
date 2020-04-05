5c9bca3da1cb10e50e74673797bba2ab516b9f83
Ricardo Medeiros <medeiros.ricardo@outlook.com>
2020-04-05 14:40:23 -0400

    Assignment 6 - Step 2 - before delete and update buttons

--- assignment6/blog/src/app/edit-post/edit-post.component.ts
+++ assignment6/blog/src/app/edit-post/edit-post.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { PostService } from '../post.service';
+import { ActivatedRoute } from '@angular/router';
+import { BlogPost } from '../../BlogPost';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-edit-post',
   templateUrl: './edit-post.component.html',
   styleUrls: ['./edit-post.component.css']
 })
-export class EditPostComponent implements OnInit {
+export class EditPostComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  blogPost: BlogPost;
+  tags: string;
+
+  private post;
+
+  constructor(private data: PostService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.post = this.route.params.subscribe(params => {
+      this.data.getPostByID(params['id']).subscribe(data => {
+        this.blogPost = data; 
+        this.tags = data.tags.toString();
+      })
+    })
+  }
+
+  ngOnDestroy() {
+    if (this.post) this.post.unsubscribe();
+  }
+
+  ngSubmit(f: NgForm) {
+    
   }
 
 }
