5332016699f6ba0b75a77e3c3235afcbe8711033
Ricardo Medeiros <medeiros.ricardo@outlook.com>
2020-04-09 16:26:37 -0400

    Assignment 6 - done

--- assignment6/blog/src/app/app.component.ts
+++ assignment6/blog/src/app/app.component.ts
@@ -1,10 +1,10 @@
 /*********************************************************************************
-*  WEB422 – Assignment 05
+*  WEB422 – Assignment 06
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
 *  No part of this assignment has been copied manually or electronically from any other source
 *  (including web sites) or distributed to other students.
 * 
-*  Name: Ricardo Medeiros Student ID: 135745180 Date: March 13, 2020
+*  Name: Ricardo Medeiros Student ID: 135745180 Date: April 9, 2020
 *
 ********************************************************************************/
 
